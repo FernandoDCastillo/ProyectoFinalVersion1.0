b0257b14e69d0debe06cdedd5fd22ed0fdd8e63f
FernandoDCastillo <al054435@uacam.mx>
2021-11-16 16:34:50 -0600

    Se hizo el login y ademas se realizaron cambios a las demas paginas

--- views/login.php
+++ views/login.php
@@ -1,1 +1,38 @@
-<h1>login</h1>
+<?php
+$estado_session = session_status();
+if($estado_session == PHP_SESSION_NONE)
+{
+session_start();
+}
+if (isset($_SESSION['loggedUserName'])) {
+    header("location: ?menu=401");
+?>
+<?php
+}else {?>
+<div class="row text-center login-page">
+	   <div class="col-md-12 login-form">
+	      <form action="./controller/login.php" method="post"> 			
+	         <div class="row">
+		    <div class="col-md-12 login-form-header">
+		       <p class="login-form-font-header">Iniciar sesion<p>
+		    </div>
+		</div>
+		<div class="row">
+		   <div class="col-md-12 login-from-row">
+		      <input name="email" type="text" placeholder="Usuario" required/>
+		   </div>
+		</div>
+		<div class="row">
+		   <div class="col-md-12 login-from-row">
+		      <input name="password" type="password" placeholder="Contraseña" required/>
+		   </div>
+		</div>
+		<div class="row">
+		   <div class="col-md-12 login-from-row">
+		      <button class="btn btn-info">Entrar</button>
+		   </div>
+		</div>
+	    </form>
+	</div>
+     </div>
+<?php } ?>
