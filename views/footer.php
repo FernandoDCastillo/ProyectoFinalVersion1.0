eef905ebb865a1684badf361bebb136d209d3e04
FernandoDCastillo <al054435@uacam.mx>
2021-11-16 12:15:59 -0600

    Se crearon las paginas base del proyecto

---
+++ views/footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <h3>Redes sociales</h3>
+            <ul>
+                <li>Facebook: Fernando Castillo</li>
+                <li>Instagram: fernando_daniel137</li>
+            </ul>
+    </footer>  
